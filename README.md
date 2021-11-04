3d10386c611c9122f502a94785b2c1486809edec
Dan Todd <57022952+dannyodd@users.noreply.github.com>
2021-11-04 17:39:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# n315-homework-seven
+ Advanced JSON for N315 Homework 7, Fall 2021
+ Has a list of lists, which can have lists added to or removed from it.
+ Clicking on any individual item in the list will generate a page with the items
+ inside that list. Items can be added to/removed from the list from this page.
+
+ ## URL Link
+ https://in-info-web4.informatics.iupui.edu/~danttodd/n315/n315-homework-seven/
+
+ ### GitHub Link
+ https://github.com/dannyodd/n315-homework-seven
---
+++ app/app.js
@@ -0,0 +1,529 @@
+var LISTS = [
+    {
+       "name":"Groceries",
+       "listItems":[
+          {
+             "name":"milk",
+             "checked":false
+          },
+          {
+             "name":"cheese",
+             "checked":false
+          },
+          {
+             "name":"oranges",
+             "checked":false
+          },
+          {
+            "name":"steak",
+            "checked":false
+          },
+          {
+            "name":"potatoes",
+            "checked":false
+          },
+          {
+            "name":"mushrooms",
+            "checked":false
+          },
+          {
+            "name":"garlic",
+            "checked":false
+          },
+          {
+            "name":"brussels sprouts",
+            "checked":false
+          },
+          {
+            "name":"butter",
+            "checked":false
+          },
+          {
+            "name":"rosemary",
+            "checked":false
+          }
+       ]
+    },
+    {
+       "name":"Camping",
+       "listItems":[
+          {
+             "name":"tent",
+             "checked":false
+          },
+          {
+             "name":"chairs",
+             "checked":false
+          },
+          {
+             "name":"bug spray",
+             "checked":false
+          },
+          {
+            "name":"stove",
+            "checked":false
+          },
+          {
+            "name":"lantern",
+            "checked":false
+          },
+          {
+            "name":"hammock",
+            "checked":false
+          },
+          {
+            "name":"sleeping bag",
+            "checked":false
+          },
+          {
+            "name":"cot",
+            "checked":false
+          },
+          {
+            "name":"first aid kit",
+            "checked":false
+          },
+          {
+            "name":"water purifier",
+            "checked":false
+          }
+       ]
+    },
+    {
+        "name":"Games",
+        "listItems":[
+           {
+              "name":"Skyrim",
+              "checked":false
+           },
+           {
+              "name":"Thief",
+              "checked":false
+           },
+           {
+              "name":"Dishonored",
+              "checked":false
+           },
+           {
+            "name":"Ghost of Tsushima",
+            "checked":false
+           },
+           {
+            "name":"Minecraft",
+            "checked":false
+           },
+           {
+            "name":"Halo",
+            "checked":false
+           },
+           {
+            "name":"Mortal Kombat",
+            "checked":false
+           },
+           {
+            "name":"Street Fighter",
+            "checked":false
+           },
+           {
+            "name":"Starcraft",
+            "checked":false
+           },
+           {
+            "name":"Fallout",
+            "checked":false
+           }
+        ]
+     },
+     {
+        "name":"Books",
+        "listItems":[
+           {
+              "name":"The Alchemist",
+              "checked":false
+           },
+           {
+              "name":"The Hobbit",
+              "checked":false
+           },
+           {
+              "name":"A Canticle for Liebowitz",
+              "checked":false
+           },
+           {
+            "name":"Dune",
+            "checked":false
+           },
+           {
+            "name":"It",
+            "checked":false
+           },
+           {
+            "name":"Pet Sematary",
+            "checked":false
+           },
+           {
+            "name":"Harry Potter",
+            "checked":false
+           },
+           {
+            "name":"A Series of Unfortunate Events",
+            "checked":false
+           },
+           {
+            "name":"The Dark Tower",
+            "checked":false
+           },
+           {
+            "name":"Grimm's Fairy Tales",
+            "checked":false
+           }
+        ]
+     },
+     {
+        "name":"Tasks",
+        "listItems":[
+           {
+              "name":"Do dishes",
+              "checked":false
+           },
+           {
+              "name":"Wash laundry",
+              "checked":false
+           },
+           {
+              "name":"Fold clothes",
+              "checked":false
+           },
+           {
+            "name":"Sweep floor",
+            "checked":false
+           },
+           {
+            "name":"Get groceries",
+            "checked":false
+           },
+           {
+            "name":"Oil change",
+            "checked":false
+           },
+           {
+            "name":"Pay bills",
+            "checked":false
+           },
+           {
+            "name":"Work out",
+            "checked":false
+           },
+           {
+            "name":"Meal prep",
+            "checked":false
+           },
+           {
+            "name":"Do homework",
+            "checked":false
+           }
+        ]
+     },
+     {
+        "name":"Movies",
+        "listItems":[
+           {
+              "name":"Alien",
+              "checked":false
+           },
+           {
+              "name":"Fight Club",
+              "checked":false
+           },
+           {
+              "name":"Halloween",
+              "checked":false
+           },
+           {
+            "name":"Texas Chainsaw Massacre",
+            "checked":false
+           },
+           {
+            "name":"Night of the Living Dead",
+            "checked":false
+           },
+           {
+            "name":"Evil Dead",
+            "checked":false
+           },
+           {
+            "name":"Cabin in the Woods",
+            "checked":false
+           },
+           {
+            "name":"The Ritual",
+            "checked":false
+           },
+           {
+            "name":"Midsommar",
+            "checked":false
+           },
+           {
+            "name":"Hereditary",
+            "checked":false
+           }
+        ]
+     },
+     {
+        "name":"TV Shows",
+        "listItems":[
+           {
+              "name":"Lost in Space",
+              "checked":false
+           },
+           {
+              "name":"The Walking Dead",
+              "checked":false
+           },
+           {
+              "name":"The 100",
+              "checked":false
+           },
+           {
+            "name":"Stargate: SG-1",
+            "checked":false
+           },
+           {
+            "name":"Star Trek",
+            "checked":false
+           },
+           {
+            "name":"Black Mirror",
+            "checked":false
+           },
+           {
+            "name":"The Twilight Zone",
+            "checked":false
+           },
+           {
+            "name":"Breaking Bad",
+            "checked":false
+           },
+           {
+            "name":"Narcos",
+            "checked":false
+           },
+           {
+            "name":"Westworld",
+            "checked":false
+           }
+        ]
+     },
+     {
+        "name":"Constellations",
+        "listItems":[
+           {
+              "name":"Gemini",
+              "checked":false
+           },
+           {
+              "name":"Pisces",
+              "checked":false
+           },
+           {
+              "name":"Scorpio",
+              "checked":false
+           },
+           {
+            "name":"Aquarius",
+            "checked":false
+           },
+           {
+            "name":"Sagittarius",
+            "checked":false
+           },
+           {
+            "name":"Leo",
+            "checked":false
+           },
+           {
+            "name":"Cancer",
+            "checked":false
+           },
+           {
+            "name":"Aries",
+            "checked":false
+           },
+           {
+            "name":"Libra",
+            "checked":false
+           },
+           {
+            "name":"Capricorn",
+            "checked":false
+           }
+        ]
+     },
+     {
+        "name":"Bands",
+        "listItems":[
+           {
+              "name":"Black Sabbath",
+              "checked":false
+           },
+           {
+              "name":"Led Zeppelin",
+              "checked":false
+           },
+           {
+              "name":"Clutch",
+              "checked":false
+           },
+           {
+            "name":"Rival Sons",
+            "checked":false
+           },
+           {
+            "name":"Mastodon",
+            "checked":false
+           },
+           {
+            "name":"Sleep",
+            "checked":false
+           },
+           {
+            "name":"Kyuss",
+            "checked":false
+           },
+           {
+            "name":"Queens of the Stone Age",
+            "checked":false
+           },
+           {
+            "name":"Starcrawler",
+            "checked":false
+           },
+           {
+            "name":"The Dead Weather",
+            "checked":false
+           }
+        ]
+     },
+     {
+        "name":"Instruments",
+        "listItems":[
+           {
+              "name":"Guitar",
+              "checked":false
+           },
+           {
+              "name":"Bass",
+              "checked":false
+           },
+           {
+              "name":"Violin",
+              "checked":false
+           },
+           {
+            "name":"Cello",
+            "checked":false
+           },
+           {
+            "name":"Saxophone",
+            "checked":false
+           },
+           {
+            "name":"Trombone",
+            "checked":false
+           },
+           {
+            "name":"Drums",
+            "checked":false
+           },
+           {
+            "name":"French Horn",
+            "checked":false
+           },
+           {
+            "name":"Piano",
+            "checked":false
+           },
+           {
+            "name":"Harmonica",
+            "checked":false
+           }
+        ]
+     }
+ ];
+
+function itemChecked(element, listIndex, itemIndex){
+    $(element).parent().toggleClass("strike");
+    let checkedValue = !LISTS[listIndex].listItems[itemIndex].checked;
+    LISTS[listIndex].listItems[itemIndex].checked = checkedValue;
+    //console.log(LISTS[listIndex].listItems[itemIndex].checked);
+}
+
+function addItem(listIndex){
+    let newItemName = $("#addItem").val();
+    let newItemObj = {
+        "name":newItemName,
+        "checked":false
+    };
+    LISTS[listIndex].listItems.push(newItemObj);
+    loadListItems(listIndex);
+}
+
+function deleteItem(listIndex, idx){
+    LISTS[listIndex].listItems.splice(idx, 1);
+    loadListItems(listIndex);
+}
+
+function loadListItems (listIndex){
+    let listString = `<h1>${LISTS[listIndex].name}</h1>
+    <button onclick="loadLists()">Back</button><ul>`;
+    $.each(LISTS[listIndex].listItems, function (idx, listItem) {
+        listString += `<li id="${idx}" class="noPointer ${
+            listItem.checked ? "strike" : ""
+        }"><input ${
+            listItem.checked ? (checked = "checked") : ""
+        } type="checkbox" id="${idx}" name="${
+            listItem.name
+        }" onclick="itemChecked(this, ${listIndex}, ${idx})"><span>${listItem.name}</span>
+        <span class="delete" onclick="deleteItem(${listIndex}, ${idx})">Delete</span> 
+        </li>`;
+    });
+    listString += `</ul>
+        <div class="addItemInput">
+        <input id="addItem" type="text">
+        <button onclick="addItem(${listIndex})">Add Item</button>
+        </div>`;
+    $("#app").html(listString);
+}
+function addList(){
+    let newListName = $("#addList").val();
+    let newListObj = {
+        "name":newListName,
+        "listItems":[]
+    };
+    LISTS.push(newListObj);
+    loadLists();
+}
+function deleteList(listIndex){
+    LISTS.splice(listIndex, 1);
+    loadLists();
+}
+function loadLists(){
+    let listString = "<h1>List of Lists</h1><ul>";
+    $.each(LISTS, function(idx, list){
+        listString += `<li id="${idx}" onclick="loadListItems(${idx})">
+        <span>${list.name}</span>
+        <span class="delete" onclick="deleteList(${idx})">Delete</span>
+        <span class="right">Items: ${list.listItems.length}</span></li>`;
+    });
+    listString += `</ul>
+    <div class="addItemInput">
+    <input id="addList" type="text">
+    <button onclick="addList()">Add List</button>
+    </div>`;
+    $("#app").html(listString);
+}
+
+
+$(document).ready(function() {
+    loadLists();
+});
